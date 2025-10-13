0941c6f8b7c8cca5977539db1c932ff1c722b899
guotai <guotaixiao87@gmail.com>
2025-10-14 10:29:25 +1100

    feat: Implement AddButton component

---
+++ app/components/AddButton.tsx
@@ -0,0 +1,9 @@
+export default function AddButton() {
+    return (
+      <div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Add Task to Todo List
+        </button>
+      </div>  
+    );
+}
