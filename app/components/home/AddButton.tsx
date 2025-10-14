88ed962b8ae015a7ab2d3131bd9e5ada047013ca
guotai <guotaixiao87@gmail.com>
2025-10-14 11:30:20 +1100

    feat: add general button

--- app/components/home/AddButton.tsx
+++ app/components/home/AddButton.tsx
@@ -1,6 +1,6 @@
 export default function AddButton() {
     return (
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full">
+        <button className="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600 w-full">
           Add Task to Todo List
         </button>
     );
--- app/components/todoList/Task.tsx
+++ app/components/todoList/Task.tsx
@@ -1,12 +1,13 @@
+import Button from "../ui/Button";
+
 export default function Task() {
   return (
     <div className="flex justify-between shadow-sm min-h-12 items-center px-4 py-2 text-black hover:bg-gray-300">
       <p className="w-1/2">go to gym </p>
-      <div className="w-1/9 flex justify-between">
-        <button>edit</button>
-        <button>delete</button>
+      <div className="w-1/7 flex justify-between">
+        <Button kind="confirm">Edit</Button>
+        <Button kind="delete">Delete</Button>
       </div>
     </div>
   );
 }
- 
