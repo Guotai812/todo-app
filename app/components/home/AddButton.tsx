f746ea55480498f6d4b49d05b64e4ca00737a9e1
guotai <guotaixiao87@gmail.com>
2025-10-14 15:02:54 +1100

    feat: enhance AddButton component with styling and label

--- app/components/home/AddButton.tsx
+++ app/components/home/AddButton.tsx
@@ -1,5 +1,12 @@
 type AddButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function AddButton(props: AddButtonProps) {
-  return <button {...props} />;
+  return (
+    <button
+      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500 w-full"
+      {...props}
+    >
+      Add Task to Todo List
+    </button>
+  );
 }
