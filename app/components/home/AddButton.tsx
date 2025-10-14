bf0ecf15761ebf2ca2123aa0d6ffa19825c3ab39
guotai <guotaixiao87@gmail.com>
2025-10-14 14:59:27 +1100

    refactor: simplify AddButton component and props definition

--- app/components/home/AddButton.tsx
+++ app/components/home/AddButton.tsx
@@ -1,12 +1,5 @@
-interface AddButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+type AddButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function AddButton(props: AddButtonProps) {
-  return (
-    <button
-      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500 w-full"
-      {...props}
-    >
-      Add Task to Todo List
-    </button>
-  );
+  return <button {...props} />;
 }
